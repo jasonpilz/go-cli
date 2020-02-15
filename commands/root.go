bbdaf04208d6984a3369692dc0d584cf7768c6a7
Jason Pilz <jasonpilz@gmail.com>
2020-02-15 10:54:20 -0800

    Add root command

---
+++ commands/root.go
@@ -0,0 +1,27 @@
+package commands
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "cli",
+	Short: "cli is a command line interface for ...",
+	Long:  "cli is a command line interface for ...",
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(0)
+	}
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
